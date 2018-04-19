5bcb3bc56bacab331270a6b379567b45d2e4ddfb
Tina <tina.fan@live.com>
2018-04-19 12:53:40 -0700

    Add search and update/edit routes and queries

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ var Client = require('mariasql')
 require('dotenv').config()
 // table
 const table = 'InventoryClient'
+const versionEditsTable = 'InvetoryClientEdits'
 
 // configuration
 /*
@@ -19,13 +20,36 @@ var c = new Client({
   db: process.env.DB_NAME
 })
 
+
+// Query the database for search. The first search will be completed on the InventoryClient
+// table for the specific search. 
+const queryForm = (profile, callback) => {
+  const sql = `SELECT * FROM ${table} WHERE ('${form.searchField}' = '${form.searchTerm}');
+  console.log('QUERY FORM SQL : ' + sql)
+}
+
+// Query the database for search. The second search will be completed on the InvetoryClientEdits
+// table for all the versions corresponding to the item. 
+const queryVersions = () => {
+
+}
+
+// Edit database entry. Edited logs are entered into the versionEditsTable, 
+// with the original entry's correspnding ID. The version logs 
+// also need to have timestamps and editedBy fields. 
+const editVersions = () => {
+
+}
+
+// New item entry is created into the InventoryClient table.
+// TODO: add unique entry ID 
 const fillForm = (form, profile, callback) => {
   const sql = `INSERT INTO ${table} VALUES ('${profile.cwl}', '${profile.shibFirstName}', '${profile.shibLastName}', 
   '${form.id}', '${form.date}', '${form.dateModified}', '${form.user}', 
   '${form.assetName}', '${form.assetModelNumber}', '${form.assetSpecs}', '${form.assetSerialNumber}', '${form.assetTag}', '${form.assetOwner}', 
   '${form.modelYear}', '${form.speedChart}', '${form.vendor}', '${form.jvNumber}', '${form.datePurchased}', '${form.currentUser}', '${form.previousUser}', 
   '${form.assetLocation}', '${form.disposalDate}', '${form.methodOfDisposal}', '${form.userType}', '${form.unitAffiliation}', '${form.cost}');`
-  console.log('SQL ' + sql)
+  console.log('FILL FORM SQL : ' + sql)
   // var prep = c.prepare(q)
 
   // Insert
@@ -42,5 +66,8 @@ const fillForm = (form, profile, callback) => {
 }
 
 export {
-  fillForm
+  fillForm,
+  queryForm,
+  queryVersions,
+  editVersions
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-import { fillForm } from './database'
+import { fillForm }, { queryForm }, { queryVersions }, { editVersions } from './database'
+
 
 var multer = require('multer')
 var upload = multer()
@@ -12,6 +13,50 @@ routes.post('/api/form', upload.array(), (req, res) => {
     shibLastName: req.headers.sn
   }
   fillForm(req.body, profile, (err, result) => {
+    if (err) { res.status(404).send(err) } else result.type === 'error' ? res.status(404).send(result) : res.status(200).send(result)
+  })
+})
+
+routes.get('/api/form', (req, res) => {
+  const profile = {
+    cwl: req.headers.cwlloginname,
+    shibFirstName: req.headers.givenname,
+    shibLastName: req.headers.sn
+  }
+  
+  queryForm(req.body, profile, (err, result) => {
+    if (err) {
+      res.status(404).send(err)
+    } else {
+      res.status(200).send(result)
+    }
+  })
+})
+
+routes.get('/api/versions', (req, res) => {
+  const profile = {
+    cwl: req.headers.cwlloginname,
+    shibFirstName: req.headers.givenname,
+    shibLastName: req.headers.sn
+  }
+  
+  queryVersions(req.body, profile, (err, result) => {
+    if (err) {
+      res.status(404).send(err)
+    } else {
+      res.status(200).send(result)
+    }
+  })
+})
+
+routes.post('/api/versions', (req, res) => {
+  const profile = {
+    cwl: req.headers.cwlloginname,
+    shibFirstName: req.headers.givenname,
+    shibLastName: req.headers.sn
+  }
+  
+  editVersions(req.body, profile, (err, result) => {
     if (err) { res.status(404).send(err) } else result.type === 'error' ? res.status(404).send(result) : res.status(200).send(result)
   })
 })
